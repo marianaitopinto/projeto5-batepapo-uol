0aa88d7ff385697250eb9739f98d187092786d06
Mariana Ito <marianaitopinto@gmail.com>
2022-02-11 23:02:02 -0300

    mensagens renderizadas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let nome = "";
 
-
 function entrarSala() {
 
     nome = prompt("Digite o seu usuário:");
@@ -14,20 +13,21 @@ function entrarSala() {
     promessa.then(entrar);
     promessa.catch(deuErro);
 
-    function entrar(status) {
-        alert("Deu certo");
-        setInterval(verificarConexaoUsuarios, 5000);
+}
+
+function entrar(status) {
+    alert("Deu certo");
+    setInterval(verificarConexaoUsuarios, 5000);
+    carregarMensagens();
+}
+
+function deuErro(erro) {
+    let statusErro = erro.response.status;
+
+    if (statusErro === 400) {
+        alert("Esse usuário já está sendo utilizado!! Utilize outro.");
+        entrarSala();
     }
-
-    function deuErro(erro) {
-        let statusErro = erro.response.status;
-
-        if (statusErro === 400) {
-            alert("Esse usuário já está sendo utilizado!! Utilize outro.");
-            entrarSala();
-        }
-    }
-
 }
 
 function verificarConexaoUsuarios() {
@@ -38,6 +38,48 @@ function verificarConexaoUsuarios() {
     );
 
     promessa.then();
+}
+
+function carregarMensagens() {
+    const mensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    mensagens.then(renderizarMensagensTela);
+    mensagens.catch(alertar);
+
+}
+
+function renderizarMensagensTela(mensagem) {
+    let dadosMensagens = mensagem.data;
+    let corpoMensagem = document.querySelector(".mensagensBatePapo");
+    console.log(dadosMensagens);
+
+    dadosMensagens.forEach(item => {
+
+        if (item.type === 'message') {
+            corpoMensagem.innerHTML += `
+            <div class="mensagemParaTodos mensagem" data-identifier="message">
+                <span>(${item.time})  </span> <b>${item.from}</b> para <b>${item.to}</b>: ${item.text}
+            </div>           
+            `
+        } else if (item.type === 'private_message') {
+
+            corpoMensagem.innerHTML += `
+            <div class="mensagemPrivada mensagem" data-identifier="message">
+            <span>(${item.time})  </span> <b>${item.from}</b> para <b>${item.to}</b>: ${item.text}
+            </div>
+            `
+        } else if (item.type === 'status') {
+            corpoMensagem.innerHTML += `
+            <div class="status mensagem" data-identifier="message">
+            <span>(${item.time})  </span> <b>${item.from}</b>  ${item.text}
+            </div>
+            `
+        }
+    })
+
+}
+
+function alertar() {
+    alert("Deu ruim no carregamento")
 }
 
 entrarSala();
